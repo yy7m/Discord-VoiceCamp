410bf96d0c70e8d52fc84686b8639b3895e48e39
Rajat Rajput <coderrajatrajput@gmail.com>
2022-08-19 09:06:21 +0530

    v1.2

--- keep_alive.py
+++ keep_alive.py
@@ -5,7 +5,7 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    return "Bot is online"
+    return "Selfbot has been turned on."
 
 def run():
   app.run(host='0.0.0.0',port=8080)
--- main.py
+++ main.py
@@ -3,15 +3,32 @@ from discord.ext import commands
 import discord
 from colorama import Fore
 from keep_alive import keep_alive
+from os import environ
+import json
+
 bot = commands.Bot("!", self_bot=True) # Defining bot.
 
 @bot.event # Turning the bot online.
 async def on_ready():
     print("This program has logged in to the account " + Fore.YELLOW + f"{bot.user}.\n")
+    with open("data.json", "r") as f:
+        data = json.load(f)
+    if data["guild"] == None or data["channel"] == None:
+        pass
+    else:
+        try:
+            voice_channel = discord.utils.get(bot.get_guild(int(data["guild"])).channels, id = int(data["channel"]))
+            await voice_channel.connect()
+            print(f"{Fore.GREEN}[-]{Fore.WHITE} Connected to {Fore.CYAN}{voice_channel} {Fore.WHITE}in {Fore.CYAN}{voice_channel.guild}{Fore.WHITE}.")
+        except:
+            print(f"{Fore.RED} [ - ] Error Occured. Please reconnect using commands.")
 
 @bot.command() # Join command.
 async def join(ctx, voice_channel : discord.VoiceChannel):
     await voice_channel.connect()
+    data = {"guild":str(ctx.guild.id),"channel":str(voice_channel.id)}
+    with open("data.json", "w") as f:
+        json.dump(data, f)
     print(f"{Fore.GREEN}[-]{Fore.WHITE} Connected to {Fore.CYAN}{voice_channel} {Fore.WHITE}in {Fore.CYAN}{voice_channel.guild}{Fore.WHITE}.")
     await ctx.message.delete()
 
@@ -19,9 +36,10 @@ async def join(ctx, voice_channel : discord.VoiceChannel):
 async def leave(ctx):
     voice_client = ctx.message.guild.voice_client
     await voice_client.disconnect()
+    with open("data.json", "w") as f:
+        json.dump({"guild":None,"channel":None}, f)
     print(f"{Fore.RED}[-]{Fore.WHITE} Disconnected from {Fore.CYAN}{voice_client.channel}{Fore.WHITE} in {Fore.CYAN}{ctx.message.guild}{Fore.WHITE}.")
     await ctx.message.delete()
-    
 
 keep_alive()
-bot.run("add_your_token_here", bot=False) # Run the bot by adding your token in between the quotes.
+bot.run(environ["token"], bot=False) # Run the bot by adding your token in between the quotes.
